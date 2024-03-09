ed2c70ec05e7b35c00667500c3f0c38fbe36c33b
PawaraLiyanage <158852235+PawaraLiyanage@users.noreply.github.com>
2024-03-09 09:54:10 -0800

    Done Patient Entity

--- src/main/java/com/medilab/entity/PatientEntity.java
+++ src/main/java/com/medilab/entity/PatientEntity.java
@@ -1,4 +1,22 @@
 package com.medilab.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="Patient")
+@Data
+
 public class PatientEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String age;
+    private String gender;
+    private String contactNumber;
+    private String email;
+    private String address;
+
 }
